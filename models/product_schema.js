38d13a3930c7ca9a414c94536e024fe55caa24f9
mythily-muthu <mythilimuthu18@gmail.com>
2023-02-15 14:03:57 +0530

    Feat: added the product schema

--- models/product_schema.js
+++ models/product_schema.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
 
+let ProductSchema = new mongoose.Schema({
+    product_name: { type: String, required: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true },
+    image_url: { type: String, required: true },
+
+});
+
 const Products = mongoose.model("Products")
+
+
 module.exports = Products
